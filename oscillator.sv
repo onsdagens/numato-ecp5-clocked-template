c7c6e22e98091ec05e19721b7c87014b7ab9d30f
onsdagens <pawel.dzialo@ltu.se>
2025-04-22 15:57:03 +0200

    clarify divider, add empty readme

--- oscillator.sv
+++ oscillator.sv
@@ -3,6 +3,7 @@ module OSC_TOP (
 );
   output clk;
   OSCG OSCinst0 (.OSC(clk));
-  // 2.4MHz
+  // 2.4MHz (http://www.latticesemi.com/view_document?document_id=50465 page 26)
+  // valid divider values 2 (~155.0MHz) - 128 (~2.4MHz)
   defparam OSCinst0.DIV = 128;
 endmodule
